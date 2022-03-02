e592576214abde8f576f80242b8ca449fb9da075
abbe42 <angler1357@gmail.com>
2022-03-02 15:35:45 -0500

    changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include <libserial/SerialPort.h>
 #include <libserial/SerialStream.h>
 
-SerialPort  serial_port;
-
-serial_port.SetBaudRate( BaudRate::BAUD__9600);
-
-char c;
-int timeout = 0;
-ReadByte(c, timeout);
+BNO_Sensor sensor;
+sensor.Reading()
