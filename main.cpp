68ce55116d6da6dabfe0a4e023f0cb38e2ba301c
mjetly1 <98354078+mjetly1@users.noreply.github.com>
2022-03-09 14:22:35 -0500

    Reading Packets in Main

--- main.cpp
+++ main.cpp
@@ -4,29 +4,53 @@
 #include <ostream>
 
 
-
+using namespace std;
 using namespace LibSerial;
+
+struct recieve_packet{
+    char header;
+    float measurement[40];
+    float orient_yaw;
+    float orient_pitch;
+    float orient_roll;
+    uint8_t checksum;
+
+};
+
 int main(void)
 {
     char c;
-     // Instantiate a Serial Port and a Serial Stream object.
-     SerialPort serial_port;
+    // Instantiate a Serial Port and a Serial Stream object.
+    SerialPort serial_port;
 
-     // Open the hardware serial ports.
-     serial_port.Open( "/dev/ttyUSB0" );
+    // Open the hardware serial ports.
+    serial_port.Open( "/dev/ttyUSB0" );
 
-     // Set the baud rates.
-     serial_port.SetBaudRate( BaudRate::BAUD_115200 );
+    // Set the baud rates.
+    serial_port.SetBaudRate( BaudRate::BAUD_9600);
+    uint8_t c;
+    recieve_packet pkt;
+    cobSerial myCOB;
 
-     char read_byte_1 = 'A';
-     char read_byte_2 = 'B';
+    while(1)
+    {
+        if (serial_port.IsDataAvailable())
+        {
+            serial.ReadByte(c, 0);
 
-     // Read a character.
-     serial_port.ReadByte(c, 1);
+            myCOB.addByte(c);
+        }
+        if (myCOB.packetAvail())
+        {
+            int len = myCOB.getPacket((char*)&pkt);
+            cout << endl;
+            cout << "Packet Length " << len << endl;
+            cout << "Header: " << pkt.header << ", " << pkt.measurement[0] << endl;
+            cout << "Value: " << pkt.measurement[23] << ", " << int(pkt.checksum) << endl;
+        }
+    }
 
-     std::cout << "serial_port read:   " << read_byte_1 << std::endl;
-
-     // Close the Serial Port and Serial Stream.
-     serial_port.Close();
-     return 0;
+    // Close the Serial Port and Serial Stream.
+    serial_port.Close();
+    return 0;
 }
