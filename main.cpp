f1438382ae7d9d8d97ee8b045b361e0d1878e507
abbe42 <angler1357@gmail.com>
2022-03-09 12:41:39 -0500

    ye

--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 #include <libserial/SerialStream.h>
 #include<iostream>
 #include<ostream>
+##include "BNO_Sensor.h"
 
-use namespace LibSerial 
+use namespace LibSerial
 int main()
 {
     char c;
      // Instantiate a Serial Port and a Serial Stream object.
-     LibSerial::SerialPort serial_port;
+     SerialPort serial_port;
 
      // Open the hardware serial ports.
      serial_port.Open( "/dev/ttyUSB0" );
 
      // Set the baud rates.
-     serial_port.SetBaudRate( LibSerial::BaudRate::BAUD_115200 );
+     serial_port.SetBaudRate( BaudRate::BAUD_115200 );
 
      char read_byte_1 = 'A';
      char read_byte_2 = 'B';
