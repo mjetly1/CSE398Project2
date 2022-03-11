6bacf7931f51d4f9ac58b8827030ede8623cd8fb
mjetly1 <98354078+mjetly1@users.noreply.github.com>
2022-03-11 17:39:05 -0500

    Update COBSdecoder.h

--- COBSdecoder.h
+++ COBSdecoder.h
@@ -7,12 +7,12 @@ class cobSerial {
 		void addByte(uint8_t c);
 		bool packetAvail();
 		int getPacket(char* ptrOutput);
-		static const int constMaxPacketSize = 512;
+		static const int constMaxPacketSize = 256;
 	private:
 		void decode();
 		bool bAvail;
 		char buff[constMaxPacketSize];
 		int buffIdx;
 		char decodedBuff[constMaxPacketSize];
-        int decodeIdx;
+		int decodeIdx;
 };
